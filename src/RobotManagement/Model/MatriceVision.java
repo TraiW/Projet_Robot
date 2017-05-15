81f0e126f5ace2c7157d7c81f61f415598680c78
JulienVera <julien.vera@hotmail.fr>
2017-05-15 12:11:28 +0200

    matrice vision

--- src/RobotManagement/Model/MatriceVision.java
+++ src/RobotManagement/Model/MatriceVision.java
@@ -5,10 +5,14 @@ public class MatriceVision {
 	private int nbLignes=mat.length;
 	private int nbColonnes=mat[0].length;
 	
+	
 	public MatriceVision(){
 		for (int i=0;i<mat.length;i++)
 			for(int j=0;j<mat[i].length;j++)
 				mat[i][j]=0;
+		
+		mat[0][0]=1;
+		
 	}
 	public int getNbLignes() {return nbLignes;}
 	public int getNbColonnes() {return nbColonnes;}
@@ -23,12 +27,24 @@ public class MatriceVision {
 			System.out.println("");
 		}
 	}
-
+	public MatriceVision rotation(MatriceVision matrice){
+		MatriceVision retour=new MatriceVision();
+		for (int i=0;i<matrice.nbColonnes;i++)
+		{
+			
+		}
+		return retour;
+	}
 	
-	
-	public static void main(String[] args) {
+	public static void main(String[] args){
 		MatriceVision mat=new MatriceVision();
 		mat.printMatrix();
+		System.out.println("");
+
+		
+		mat.printMatrix();
+
+		
 		System.out.println(mat.getNbLignes()+" lign "+mat.getNbColonnes()+" col");
 	}
 }
