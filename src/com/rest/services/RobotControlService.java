7b495489271b4b4076b6d499dd07ea7378829913
JeanMangholz <jean.mangholz@cpe.fr>
2017-05-31 15:57:35 +0200

    MAj unlock lock

--- src/com/rest/services/RobotControlService.java
+++ src/com/rest/services/RobotControlService.java
@@ -29,7 +29,7 @@ public class RobotControlService {
 	RobotCtr robotCtr = new RobotCtr(robot.getEnv_decouvert(), robot);
 	private Case[][]tabEnv = null;
 	private String[]tabAutoMapping=null;
-	private static boolean unlock=true;
+	private static boolean unlock=false;
 	//Inject servlet context (needed to get general context, application memory space, session memory space ...)
 	@Context
 	ServletContext context;
